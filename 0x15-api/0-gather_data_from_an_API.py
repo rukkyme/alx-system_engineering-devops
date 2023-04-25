73ef9b3791d832bca8ed35c2aad4cfcb182b7273
root <root@DESKTOP-5585RD7>
2023-04-25 13:45:12 +0100

    API, gather data

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,31 +1,17 @@
 #!/usr/bin/python3
 import requests
-
-# Function to get employee TODO list progress
-def get_employee_todo_progress(employee_id):
-    # API endpoint URL
-    url = f'https://jsonplaceholder.typicode.com/users/{employee_id}/todos'
-
-    # Send GET request to the API
-    response = requests.get(url)
-
-    if response.status_code == 200:
-        todos = response.json()
-        employee_name = todos[0]['name'].split()[0]
-        total_tasks = len(todos)
-        completed_tasks = sum(1 for todo in todos if todo['completed'])
-        print(f'Employee {employee_name} is done with tasks ({completed_tasks}/{total_tasks}):')
-        for todo in todos:
-            if todo['completed']:
-                print(f'\t{todo["title"]}')
-    else:
-        print(f'Error getting employee TODO list progress. Response status code: {response.status_code}')
-
+import sys
 
 if __name__ == '__main__':
-    # This accept employee ID as input
-    employee_id = int(input('Enter employee ID: '))
+    url = 'https://jsonplaceholder.typicode.com/'
+    usr_id = requests.get(url + 'users/{}'.format(sys.argv[1])).json()
+    to_do = requests.get(url + 'todos', params={'userId': sys.argv[1]}).json()
 
-    # this calls the function with the provided employee ID
-    get_employee_todo_progress(employee_id)
+    completed = [title.get("title") for title in to_do if
+                 title.get('completed') is True]
+    print(completed)
+    print("Employee {} is done with tasks({}/{}):".format(usr_id.get("name"),
+                                                          len(completed),
+                                                          len(to_do)))
+    [print("\t {}".format(title)) for title in completed]"
 
