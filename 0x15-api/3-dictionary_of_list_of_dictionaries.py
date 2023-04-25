748a81f856bfbb9ee6292e953fde19545c572976
root <root@DESKTOP-5585RD7>
2023-04-25 16:11:16 +0100

    task 3

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""This expo:rts to-do list information of all employees to JSON format."""
+import json
+import requests
+
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com/"
+    users = requests.get(url + "users").json()
+
+    all_user_data = {}
+    for user in users:
+        user_id = user.get("id")
+        username = user.get("username")
+        todos = requests.get(url + "todos", params={"userId": user_id}).json()
+        user_data = [{
+            "task": t.get("title"),
+            "completed": t.get("completed"),
+            "username": username
+        } for t in todos]
+        all_user_data[user_id] = user_data
+
+    with open("todo_all_employees.json", "w") as jsonfile:
+        json.dump(all_user_data, jsonfile)i
