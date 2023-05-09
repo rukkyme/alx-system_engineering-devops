f18c87c2ecdf347206958307eae1e49e9e51c52a
rukkyme <communicatejoy@yahoo.com>
2023-05-09 23:31:33 +0100

    top 10 hot_post

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
+"""Print the titles of the first 10 hot posts for a given subreddit."""
+
 import requests
 
 def top_ten(subreddit):
-    """Print the titles of the first 10 hot posts for a given subreddit."""
+    
     url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
     headers = {
         "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
