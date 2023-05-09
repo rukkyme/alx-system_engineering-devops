caa26a029836f99b4f1150fe0717e655113e1d16
rukkyme <communicatejoy@yahoo.com>
2023-05-10 00:11:52 +0100

    accessing Reddit's API

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,12 +1,25 @@
 #!/usr/bin/python3
+"""
+    This script returns the number of subscribers for a given subreddit.
+
+    Args:
+        subreddit (str): The name of the subreddit.
+
+    Returns:
+        int: The total number of subscribers for the given subreddit.
+    """
 import requests
 
 def number_of_subscribers(subreddit):
-    """Return the total number of subscribers on a given subreddit."""
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {"User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"}
-    response = requests.get(url, headers=headers)
-    response.raise_for_status()
-    return response.json()["data"]["subscribers"]
+    headers = {"User-Agent": "MyBot/0.0.1"}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+
+    if response.status_code == 200:
+        data = response.json().get("data")
+        return data.get("subscribers")
+    else:
+        return 0
+
 
 
