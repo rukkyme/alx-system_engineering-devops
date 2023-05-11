50523951ca01096e122551395de6667beb21c00c
rukkyme <communicatejoy@yahoo.com>
2023-05-11 18:26:28 +0100

    debugging with puppet

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,11 +1,6 @@
 # fixing a bug using puppet
 
-$fix_php_extension_issue = {
-  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-  path    => ['/usr/local/bin', '/bin'],
-}
-
 exec { 'fix the php extension issue':
-  * => $fix_php_extension_issue,
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
 }
-
