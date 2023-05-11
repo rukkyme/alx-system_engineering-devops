b18bfe68d6d97c174997e2cfc5ccb716070c77ae
rukkyme <communicatejoy@yahoo.com>
2023-05-11 20:22:35 +0100

    Top Ten

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,22 +1,44 @@
 #!/usr/bin/python3
-"""Print the titles of the first 10 hot posts for a given subreddit."""
+
+"""
+This function query Reddit API and print the titles
+of the first 10 hot posts listed for a given subreddit.
+"""
 
 import requests
+
+
 def top_ten(subreddit):
-    url = f"https://www.reddit.com/r/{subreddit}/hot/.json"
-    headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
-    }
-    params = {
-        "limit": 10
-    }
-    response = requests.get(url, headers=headers, params=params,
-                            allow_redirects=False)
-    if response.status_code != requests.codes.ok:
-        print("None")
+    """
+    Query Reddit API and print the titles of the first 10 hot posts
+    for a given subreddit.
+
+    Args:
+        subreddit (str): The name of the subreddit to query.
+
+    Returns:
+        None
+
+    Prints:
+        title of the first 10 hot posts, or 'None' if subreddit invalid.
+
+    Raises:
+        None
+    """
+    # Set custom User-Agent header to avoid Too Many Requests error
+    headers = {'User-Agent': 'rukkyme/0.1'}
+    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
+    response = requests.get(url, headers=headers, allow_redirects=False)
+
+    # If the subreddit is invalid, print None
+    if response.status_code == 404:
+        print(None)
         return
-    results = response.json().get("data")
-    for child in results.get("children"):
-        print(child.get("data").get("title"))
 
-
+    if response.status_code == 200:
+        data = response.json()
+        posts = data['data']['children']
+        for post in posts[:10]:
+            print(post['data']['title'])
+    else:
+        print("Error: {}".format(response.status_code))
